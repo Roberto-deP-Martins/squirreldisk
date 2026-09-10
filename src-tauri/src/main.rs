3d1080ad5214118553ae1c50b412d11e2b687e8a
Roberto Martins <roberto.martinsps3@gmail.com>
2026-09-10 01:37:44 -0300

    remove @tauri-apps/plugin-fs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -35,7 +35,6 @@ fn main() {
         
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_os::init())     
-        .plugin(tauri_plugin_fs::init())     
         .plugin(tauri_plugin_dialog::init()) 
         
         .manage(MyState(Default::default()))
